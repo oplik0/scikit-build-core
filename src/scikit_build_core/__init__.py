184f9f1c48ceb2e8f05b6944a5a3368f5df37848
Jakub Bliźniuk <opliko.reg@protonmail.com>
2025-10-22 13:42:46 +0200

    fix: static __version__

--- src/scikit_build_core/__init__.py
+++ src/scikit_build_core/__init__.py
@@ -6,6 +6,6 @@ scikit-build-core: PEP 517 builder for Scikit-Build
 
 from __future__ import annotations
 
-from ._version import version as __version__
+__version__ = "0.11.7"
 
 __all__ = ["__version__"]
